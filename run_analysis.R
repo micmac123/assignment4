f80c0c2955942d6b793c3b6c4fe162f57b9ef950
micmac123 <a256138@fmr.com>
2016-05-07 15:06:43 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 # Merge the training and test sets
-
+    library(plyr)
 
     if(!file.exists("./data")){dir.create("./data")}
     fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
